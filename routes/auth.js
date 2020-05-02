bcb4fcd78e87cfd0ab5160a86f990ced87cac6a7
Syndicate555 <saziz028@uottawa.ca>
2020-05-02 17:40:49 -0400

    changes made

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,8 @@ const router = express.Router();
 // @desc Get logged in user
 // @access Private
 
-router.post("/", (req, res) => {
-  res.send("Register a user");
+router.get("/", (req, res) => {
+  res.send("Get logged in user");
 });
 
 // @route POST api/users
--- routes/contacts.js
+++ routes/contacts.js
@@ -13,8 +13,24 @@ router.get("/", (req, res) => {
 // @desc Add new contact
 // @access Private
 
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
+router.post("/", (req, res) => {
+  res.send("Add contact");
+});
+
+// @route PUT api/contacts/:id
+// @desc Update a contact
+// @access private
+
+router.put("/:id", (req, res) => {
+  res.send("Update contact");
+});
+
+// @route DELETE api/contacts/:id
+// @desc Delete contact
+// @access private
+
+router.delete("/:id", (req, res) => {
+  res.send("Delete contact");
 });
 
 module.exports = router;
