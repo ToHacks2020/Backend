32f6c51389727cfb487059f02e1da9db902d6e34
Syndicate555 <saziz028@uottawa.ca>
2020-05-02 20:51:02 -0400

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
 
 // @route GET api/users
 // @desc Get logged in user
