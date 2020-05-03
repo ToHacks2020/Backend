67d742e900b91f95d30b9ecb5fe6f0e207f11a90
Syndicate555 <saziz028@uottawa.ca>
2020-05-02 22:13:34 -0400

    changes

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("contact", ContactSchema);
